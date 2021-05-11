10679ebf04566d21902583c63cb48bba6567ba2b
Megaselvinadengak <56498195+megaselvinadengak@users.noreply.github.com>
2021-05-12 00:00:49 +0700

    Add files via upload

---
+++ php_dasar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>PHP Dasar</title>
+</head>
+<body>
+ <h1>Belajar PHP Dasar</h1>
+ <?php
+ echo "Hello World";
+ ?>
+ <body>
+ <h2>Menggunakan Variabel</h2>
+ <?php
+$nim = "311910130";
+$nama = 'Mega S Dengak';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
+</body>
+</html>
